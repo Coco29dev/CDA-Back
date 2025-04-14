1ac7d49dacde12842849ed6f9ea777b9529e3923
Coco29dev <10059@holbertonstudents.com>
2025-04-14 15:56:25 +0200

    update app pour cors

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const sequelize = require('./config/database');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://coco29dev.github.io', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/v1/blagues', blagueRoute);
 
