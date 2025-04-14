f9645a9b098dcd5dd7032893db1ce9dc650979f7
Coco29dev <10059@holbertonstudents.com>
2025-04-14 16:06:53 +0200

    modification ordre route random

--- routes/v1/blagueRoute.js
+++ routes/v1/blagueRoute.js
@@ -4,8 +4,8 @@ const blagueController = require('../../controllers/blagueController');
 
 router.post('/', blagueController.createBlague);
 router.get('/', blagueController.getAllBlague);
-router.get('/:id', blagueController.getBlagueID);
 router.get('/random', blagueController.getBlagueRandom);
+router.get('/:id', blagueController.getBlagueID);
 router.put('/:id', blagueController.updateBlague);
 router.delete('/delete/:id', blagueController.deleteBlague);
 
