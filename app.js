6d69453cb3716707abfa60c918f3ad768800a09a
Coco29dev <10059@holbertonstudents.com>
2025-04-14 16:27:22 +0200

    update cors origin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const sequelize = require('./config/database');
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
